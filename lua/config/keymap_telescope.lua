994c2b7d6b47159149fe1460095b5e9d43c47f89
MihneaTs1 <stoicamihnea2009@gmail.com>
2025-06-12 18:55:39 +0300

    Fix key mappings for Telescope and ToggleTerm to use Ctrl instead of Command

--- lua/config/keymap_telescope.lua
+++ lua/config/keymap_telescope.lua
@@ -1,9 +1,9 @@
 return function(keymap)
-  keymap.set('n', "<D-p>", function()
+  keymap.set('n', "<C-p>", function()
     vim.cmd("Telescope fd")
   end, { noremap = true, silent = true })
 
-  keymap.set('n', "<D-S-p>", function()
+  keymap.set('n', "<C-S-p>", function()
     vim.cmd("Telescope")
   end, { noremap = true, silent = true })
 end
--- lua/config/keymap_toggleterm.lua
+++ lua/config/keymap_toggleterm.lua
@@ -1,5 +1,5 @@
 return function(keymap)
-  keymap.set({'n', 't'}, '<C-t>', function()
+  keymap.set({'n', 't'}, '<A-t>', function()
     require('toggleterm').toggle()
   end, { noremap = true, silent = true, desc = 'Toggle terminal' })
   keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true, desc = 'Exit terminal mode' })
